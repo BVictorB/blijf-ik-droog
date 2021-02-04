b651fc827c045d905d3367cba65e64b60f78f3c9
bvictorb <victory-boucher@hotmail.com>
2021-02-04 16:16:41 +0100

    added form where you can choose the coords

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,44 @@
 const 
   apiKey = 'c6d3274323a8201fd1939ef229fc0078',
-  lat = '55.860916', // Coords of Glasgow (because it always rains there)
-  lon = '-4.251433',
   exclude = 'current,hourly,daily,alerts',
   list = document.querySelector('ul'),
-  url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}`
+  form = document.querySelector('form')
 
-// API call for current weather
-// const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-fetch(url)
-  .then(
-    (res) => {
-      res.json().then((data) => {
-        console.log(data)
-        displayAllMinuteData(data.minutely)
-      })
-    }
-  )
-  .catch((err) => {
-    console.log(`Error: ${err}`)
-  })
+  const 
+    inputs = e.target.querySelectorAll('input'),
+    lat = inputs[0].value,
+    lon = inputs[1].value
+
+  fetchWeatherData(lat, lon)
+})
+
+const fetchWeatherData = (lat, lon) => {
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}`
+
+  fetch(url)
+    .then(
+      (res) => {
+        res.json().then((data) => {
+          displayAllMinuteData(data.minutely)
+        })
+      }
+    )
+    .catch((err) => {
+      console.log(`Error: ${err}`)
+    })
+}
 
 const displayAllMinuteData = (data) => {
-  const d = new Date()
+  const t = new Date()
 
   data.forEach((data, index) => {
     const 
-      minute = d.getMinutes() + index >= 60 ? d.getMinutes()+index - 60 : d.getMinutes() + index,
+      minute = t.getMinutes() + index >= 60 ? t.getMinutes()+index - 60 : t.getMinutes() + index,
       formattedMinute = minute < 10 ? `0${minute}` : minute,
-      hour = d.getMinutes() + index >= 60 ? d.getHours() + 1 : d.getHours()
+      hour = t.getMinutes() + index >= 60 ? t.getHours() + 1 : t.getHours()
 
     const li = document.createElement('li')
     li.innerHTML = `${hour}:${formattedMinute}: ${data.precipitation}mm`
